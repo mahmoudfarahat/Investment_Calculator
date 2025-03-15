6f66c1633834e8382808d24aeff2f7a84e0bf523
Mahmoud Farahat <mahmoud.ped@gmail.com>
2025-03-15 03:31:08 +0200

    1.3

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,6 +1,6 @@
 import { InvestmentService } from './../investment.service';
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-result',
@@ -13,9 +13,11 @@ export class InvestmentResultComponent {
 
 private investmentService = inject(InvestmentService);
 
-get results(){
-  return this.investmentService.resultsData;
-}
+// get results(){
+//   return this.investmentService.resultsData;
+// }
+// results = computed(() => this.investmentService.resultsData())
 
+results = this.investmentService.resultsData.asReadonly();
 
 }
